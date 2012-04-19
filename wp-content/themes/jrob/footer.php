813959df40741a5ae309bf2311a9fadba36819ac
callinmullaney <cmullaney@augustash.com>
2012-04-19 16:59:41 -0500

    theme commit

---
+++ wp-content/themes/jrob/footer.php
@@ -0,0 +1,97 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the id=main div and all content
+ * after.  Calls sidebar-footer.php for bottom widgets.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Ten
+ * @since Twenty Ten 1.0
+ */
+?>
+	<div id="footer" role="contentinfo">
+		<div id="colophon">
+					<!-- addthis button -->
+			<div class="addbtn-holder">
+				<blockquote>
+				  <blockquote>
+				    <p><a class="addthis_button" href="http://www.addthis.com/bookmark.php?v=250&amp;username=takeittoeleven"><img src="http://s7.addthis.com/static/btn/v2/lg-share-en.gif" width="125" height="16" alt="Bookmark and Share" /></a>
+			        </p>
+			      </blockquote>
+		      </blockquote>
+			</div>
+			<!-- promo-area -->
+
+<a href="http://jrobinsoncamps.com/learn-to-earn/"><img src="http://jrobinsoncamps.com/wp-content/uploads/2012/02/legendaryshirt3.png" alt="I DID IT SHIRT"/></a> 	  
+
+			<!-- bottom-section -->
+							
+				
+				<!-- columns -->
+                
+                            <div class="columns">
+                            <p>Dates &amp; Locations</p>
+								<div class="col">
+									<h3>INTENSIVE WRESTLING CAMPS</h3>
+									<?php echo events_sidebar(10, 1);?>
+								</div>
+								<div class="col">
+									<h3>5-DAY TECHNIQUE WRESTLING CAMPS</h3>
+									<?php echo events_sidebar(10, 2);?>
+                                </div>
+                               	<div style="clear: both"></div>                                     
+							</div>
+
+</div>
+				<div class="sponsors"><a href="http://www.matmanwrestling.com/" title="MATMAN"><img src="/images/2537.png" alt="Matman" /></a></div>
+
+<div class="bottom-section">
+				<div class="toolbar">
+					<ul class="links">
+						<li><a href="http://www.facebook.com/JRobCamps" target="_blank"><img src="<?php print get_stylesheet_directory_uri() ?>/images/img06.gif" alt="image06" /></a></li>
+						<li><a href="http://twitter.com/JRobIntensive" target="_blank"><img src="<?php print get_stylesheet_directory_uri() ?>/images/img07.gif" alt="image06" /></a></li>
+						<li><a href="http://www.youtube.com/user/jrobinsoncamps" target="_blank"><img src="<?php print get_stylesheet_directory_uri() ?>/images/img08.gif" alt="image06" /></a></li>
+						<li><a href="http://www.myspace.com/jrobwrestlingcamps" target="_blank"><img src="<?php print get_stylesheet_directory_uri() ?>/images/img09.gif" alt="image06" /></a></li>
+					</ul>
+					
+					<div class="subscribe">
+						<form action="http://jrobinsoncamps.us1.list-manage.com/subscribe/post?u=c294c3a9fd3f3fb7e3008b320&amp;id=fc399ae567" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank">
+							<fieldset>
+								<legend>subscribe</legend>
+								<span>WANT MORE INFO?</span>
+								<div class="row">
+									<input type="text" class="txt" title="email"  value="" name="EMAIL" id="mce-EMAIL" />
+									<input type="submit" class="sbm" value="Subscribe" name="subscribe" id="mc-embedded-subscribe"/>
+								</div>
+							</fieldset>
+						</form>
+                   </div>
+
+			</div>
+            <div class="footer-links">
+                <span class="copyright">&copy; <?php echo date("Y"); ?> J Robinson Inc. All rights reserved.</span>
+                  <?php wp_nav_menu('menu=Footer'); ?>
+            </div>
+		</div><!-- #colophon -->
+	</div><!-- #footer -->
+</div><!-- #wrapper -->
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+try {
+var pageTracker = _gat._getTracker("UA-220435-1");
+pageTracker._trackPageview();
+} catch(err) {}</script>
+<?php
+	/* Always have wp_footer() just before the closing </body>
+	 * tag of your theme, or you will break many plugins, which
+	 * generally use this hook to reference JavaScript files.
+	 */
+
+	wp_footer();
+?>
+</body>
+</html>
---
+++ wp-content/themes/jrob/header copy.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * The Header for our theme.
+ *
+ * Displays all of the <head> section and everything up till <div id="main">
+ *
+ * @package WordPress
+ * @subpackage Twenty_Ten
+ * @since Twenty Ten 1.0
+ */
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>" />
+<title><?php
+	/*
+	 * Print the <title> tag based on what is being viewed.
+	 * We filter the output of wp_title() a bit -- see
+	 * twentyten_filter_wp_title() in functions.php.
+	 */
+	wp_title( '|', true, 'right' );
+
+	?></title>
+
+<meta name="description" content="J Robinson Wrestling Camps has 34 years experience producing elite athletes. We ensure your wrestling camp experience will improve your wrestling skills and your life."/>
+
+<link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
+<link rel="profile" href="http://gmpg.org/xfn/11" />
+<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+<?php
+	/* We add some JavaScript to pages with the comment form
+	 * to support sites with threaded comments (when in use).
+	 */
+	if ( is_singular() && get_option( 'thread_comments' ) )
+		wp_enqueue_script( 'comment-reply' );
+
+	/* Always have wp_head() just before the closing </head>
+	 * tag of your theme, or you will break many plugins, which
+	 * generally use this hook to add elements to <head> such
+	 * as styles, scripts, and meta tags.
+	 */
+	wp_head();
+?>
+	<link href="<?php print get_stylesheet_directory_uri() ?>/css/lightbox.css" rel="stylesheet" type="text/css" media="screen" />
+	<!--[if IE 8]>
+    <link rel="stylesheet" type="text/css" media="all" href="<?php print get_stylesheet_directory_uri() ?>/css/ie8.css" />
+	<![endif]-->
+	<script type="text/javascript" src="<?php print get_stylesheet_directory_uri() ?>/js/swfobject.js"></script>
+	<script type="text/javascript">
+		swfobject.embedSWF('<?php print get_stylesheet_directory_uri() ?>/swf/home-flash.swf', 'placeholder-1', "848", "269", '9.0.0', '<?php print get_stylesheet_directory_uri() ?>/swf/expressInstall.swf', {}, { wmode: 'transparent' }, {});
+	</script>
+	<script src="<?php print get_stylesheet_directory_uri() ?>/js/prototype.js" type="text/javascript"></script>
+	<script src="<?php print get_stylesheet_directory_uri() ?>/js/scriptaculous.js?load=effects" type="text/javascript"></script>
+    <script src="<?php print get_stylesheet_directory_uri() ?>/js/lightbox++.js" type="text/javascript"></script>
+</head>
+
+<body <?php body_class(); ?>>
+<div id="wrapper" class="hfeed">
+	<h1 class="logo"><a href="/">J Robinson</a></h1>
+    <!-- top-bar -->
+    <div class="top-bar">
+        <?php wp_nav_menu('menu=Top Menu'); ?>
+    </div>
+    <div class="clearer"></div>
+	<div id="header">
+    <!-- main-nav -->
+        <strong class="slogan">INTENSIVE WRESTLING CAMPS</strong>
+        <strong class="sub-text">#1 Intensive camp since 1978</strong>
+        <?php wp_nav_menu( array( 'container_class' => 'menu-header', 'theme_location' => 'primary' ) ); ?>
+        <?php /*?><?php wp_page_menu( $args ); ?><?php */?> 
+    </div>
+    
+		
+	</div><!-- #header -->
+
+	
